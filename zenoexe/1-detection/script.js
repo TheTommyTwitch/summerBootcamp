0476e0d08ca89e6b938d4b258886dcda11dd50c0
Thomas Asadurian <Tasadurian@gmail.com>
2015-06-30 13:19:18 -0700

    html5 practice

---
+++ zenoexe/1-detection/script.js
@@ -0,0 +1,9 @@
+sql = document.querySelector('.websqldatabase')
+
+if(sql){
+	document.querySelector('html').style.backgroundColor = 'green';
+}
+if(!sql){
+	document.querySelector('html').style.backgroundColor = 'red';
+}
+
