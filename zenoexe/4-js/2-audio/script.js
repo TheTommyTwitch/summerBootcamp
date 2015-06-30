f9f5efd7053d5df0d214e9a17b25d7dc8aba1510
Thomas Asadurian <Tasadurian@gmail.com>
2015-06-30 15:49:39 -0700

    audio changes

--- zenoexe/4-js/2-audio/script.js
+++ zenoexe/4-js/2-audio/script.js
@@ -5,22 +5,22 @@ var down = document.querySelector('.volumeDown');
 var audio = document.querySelector('audio');
 audio.volume = .5;
 
-play.addEventListener('click', function(){
+play.addEventListener('click', function(e){
 	audio.play();
 });
 
-pause.addEventListener('click', function(){
+pause.addEventListener('click', function(e){
 	audio.pause();
 });
 
-up.addEventListener('click', function(){
-	if(audio.volume >= 0 && audio.volume <= 1){
-	audio.volume = audio.volume + (.1);
+up.addEventListener('click', function(e){
+	if(audio.volume < 1){
+	audio.volume += .1;
 	}
 });
 
-down.addEventListener('click', function(){
-	if(audio.volume >= 0 && audio.volume <=1){
-	audio.volume = audio.volume - (.1);
+down.addEventListener('click', function(e){
+	if(audio.volume > 0.1){
+	audio.volume -= .1;
 	}
 });
