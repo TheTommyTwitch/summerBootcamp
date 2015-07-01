3dbb63c16c21d69cec78f68b1111c151b764a519
Thomas Asadurian <Tasadurian@gmail.com>
2015-06-30 20:39:40 -0700

    fixed video time

--- zenoexe/4-js/3-video/script.js
+++ zenoexe/4-js/3-video/script.js
@@ -2,18 +2,23 @@ var play = document.querySelector('.play');
 var pause = document.querySelector('.pause');
 var up = document.querySelector('.volumeUp');
 var down = document.querySelector('.volumeDown');
-var down = document.querySelector('.replay');
+var replay = document.querySelector('.replay');
 var video = document.querySelector('video');
+var time = document.querySelector('.time');
 video.volume = .5;
 
-video.addEventListener('timeupdate', function() {
-    currentTimeDisplay = document.createElement('p');
-    durationDisplay = document.createElement('p');
-	currentTimeDisplay.innerHTMl = (video.timeupdate)
-	durationDisplay.innerHTML += (video.duration);
-	document.querySelector('.container').appendChild(durationDisplay);
-	document.querySelector('.container').appendChild(currentTimeDisplay);
-});
+	(function () {
+        var currentTime = 0;
+        var durationTime = 0;
+        video.addEventListener('loadedmetadata', function () {
+            durationTime = parseInt(video.duration, 10);
+            console.log(durationTime);
+        });
+        video.addEventListener('timeupdate', function () {
+            currentTime = parseInt(video.currentTime, 10);
+            time.textContent = currentTime + ' / ' + durationTime;
+        });
+    }());
 
 
 
